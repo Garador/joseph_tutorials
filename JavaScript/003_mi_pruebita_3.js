69b631ed1bab33d1e9ed22e239b65957a0984ee4
Foxman12 <foxman12minecraft@gmail.com>
2016-11-27 23:54:11 -0400

    si yo fuera un reno, cual seria?

---
+++ JavaScript/003_mi_pruebita_3.js
@@ -0,0 +1,23 @@
+function Hola() {
+
+window.alert("problemas");
+
+}
+
+function Hola2() {
+
+document.write("<b>Has las siguientes cosas.</b>");
+
+}
+
+function Hello() {
+
+document.getElementById("polla").innerHTML = "Fiesta Fiesta y Pluma Pluma...";
+
+}
+
+function NoTengoIdeas() {
+
+console.log("quieres caca?");
+
+}
