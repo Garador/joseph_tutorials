a66df7ba73dadf541cc9d1b4632a28f6b838ae8d
Foxman12 <foxman12minecraft@gmail.com>
2016-11-28 10:57:41 -0400

    lo logre

---
+++ JavaScript/004_Syntaxis.js
@@ -0,0 +1,110 @@
+{
+
+//incio de codigo
+
+  var Prueba1 = 2;
+
+  var Prueba2 = 1;
+
+  var Prueba3 = 3.9;
+
+  var Prueba4 = 8.1;
+
+  var Prueba5 = 5.5;
+
+  var Prueba6 = 7.3;
+
+//fin de codigo
+  {
+
+    Pruba1 = 5;
+
+    Prueba2 = 4;
+
+    Prueba3 = 6.4;
+
+    Prueba4 = 7.7;
+
+    Prueba5 = 6.5;
+
+    Prueba6 = 7.9;
+
+  }
+
+  {
+
+      var Cadena1 = "Cadena1";
+
+      var Cadena2 = "Cadena2";
+
+      var Cadena3 = "Cadena3";
+
+      var Cadena4 = "Cadena4";
+
+      var Cadena5 = "Cadena5";
+
+      }
+
+      {
+
+      Cadena1 = Funcional(Cadena1);
+
+      Cadena2 = Funcional(Cadena2);
+
+      Cadena3 = Funcional(Cadena3);
+
+      Cadena4 = Funcional(Cadena4);
+
+      Cadena5 = Funcional(Cadena5);
+
+      console.log(Cadena1);
+
+      console.log(Cadena2);
+
+      console.log(Cadena3);
+
+      console.log(Cadena4);
+
+      console.log(Cadena5);
+
+      console.log(DeVuelta("pt2"));
+
+      console.log(DeVuelta("hola"));
+
+      console.log(DeVuelta("pol"));
+
+      console.log(DeVuelta(Cadena1));
+
+      console.log(DeVuelta(Cadena2));
+  }
+
+
+
+
+      }
+
+      function Funcional(p1) {
+
+        Cadena5 = "p3";
+
+      Adelante();
+
+        return p1+" ...";
+
+
+
+      }
+
+function DeVuelta(pt2) {
+
+return pt2;
+
+}
+
+function Adelante() {
+
+console.log(Cadena2);
+
+return Cadena2;
+
+}
